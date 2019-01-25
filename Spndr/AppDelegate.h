d965eacff56d9d8b72c2d1b6f0440ffa037e8a95
ahmed <ahmed@Ahmed-Nader.local>
2019-01-25 23:23:43 +0200

    Initial Commit

---
+++ Spndr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spndr
+//
+//  Created by ahmed on 1/25/19.
+//  Copyright © 2019 ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spndr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spndr
+//
+//  Created by ahmed on 1/25/19.
+//  Copyright © 2019 ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
